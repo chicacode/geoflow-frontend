03a10e06db947a55d8959978286c93b481ff8fdb
chicacode <gerahernan.al@gmail.com>
2023-06-14 19:28:51 -0700

    fix: id bug

--- src/components/ProjectForm.jsx
+++ src/components/ProjectForm.jsx
@@ -37,7 +37,7 @@ const ProjectForm = () => {
       return;
     }
 
-    await submitProject({ name, description, dateDelivered, client });
+    await submitProject({ id, name, description, dateDelivered, client });
     setId(null);
     setName("");
     setDescription("");
